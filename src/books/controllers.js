b073488ad534049609bae8dc029091b954658abf
Michael Gregory <mr.m.gregory@outlook.com>
2024-02-13 14:55:48 +0000

    enhancement: add post route for adding book

---
+++ src/books/controllers.js
@@ -0,0 +1,21 @@
+const Book = require("./model");
+
+const addBook = async (req, res) => {
+  try {
+    const book = await Book.create({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+    });
+
+    res
+      .status(201)
+      .json({ message: `${book.title} was added successfully`, book: book });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+module.exports = {
+  addBook: addBook,
+};
