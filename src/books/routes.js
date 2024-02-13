148e9ec1d14e83480218004829b5e90e9a5eef33
Michael Gregory <mr.m.gregory@outlook.com>
2024-02-13 15:15:18 +0000

    enhancement: added routes and post get put delete

--- src/books/routes.js
+++ src/books/routes.js
@@ -1,8 +1,20 @@
 const { Router } = require("express");
 const bookRouter = Router();
 
-const { addBook } = require("./controllers");
+const {
+  addBook,
+  getAllBooks,
+  updateAuthor,
+  deleteBook,
+} = require("./controllers");
 
 bookRouter.post("/books/addBook", addBook);
+
+module.exports = bookRouter;
+
+bookRouter.post("/books/addBook", addBook);
+bookRouter.get("/books/getAllBooks", getAllBooks);
+bookRouter.put("/books/updateAuthor", updateAuthor);
+bookRouter.delete("/books/deleteBook", deleteBook);
 
 module.exports = bookRouter;
