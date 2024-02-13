6737e78d24d78dff036d31e1ed99aeb1c3c2045d
Michael Gregory <mr.m.gregory@outlook.com>
2024-02-13 13:49:29 +0000

    chore: add db connection

---
+++ src/books/model.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+
+const sequelize = require("../db/connection");
+
+const Book = sequelize.define("Book", {
+  title: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  author: {
+    type: DataTypes.STRING,
+    unique: true,
+    defaultValue: "some author",
+  },
+  genre: {
+    type: DataTypes.STRING,
+    defaultValue: "some genre",
+  },
+});
+
+module.exports = Book;
